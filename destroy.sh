e3d28167de1c7b2e59c03e6ee55bd6d56b49b555
Mark McFate <mcfatem@grinnell.edu>
2020-05-17 19:34:03 -0500

    Fixed typo in destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
+rm -f /opt/containers/traefik/data/acme.json
+# 
 docker stop $(docker ps -q);
 docker rm -v $(docker ps -qa);
-# docker image rm $(docker image ls -q)
+docker image rm $(docker image ls -q)
 docker system prune --force
 #
-rm -v /opt/contianers/traefik/data/acme.json
 touch /opt/containers/traefik/data/acme.json
 chmod 600 /opt/containers/traefik/data/acme.json
